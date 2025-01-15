4b14735d79c2f2448e60d9fce80cedf4e20142b2
Nicolás Sort Sauquillo <nicolassortsauquillo@gmail.com>
2025-01-15 17:59:58 +0100

    he tocao un par de parametros del inspector del flocking

--- Lab_Final/Assets/Scripts/FlockManager.cs
+++ Lab_Final/Assets/Scripts/FlockManager.cs
@@ -29,35 +29,46 @@ public class FlockManager : MonoBehaviour
     public int leaderInfluence;
 
     // Start is called before the first frame update
+    // This method initializes the pokemons by spawning them within the defined limits
     void Start()
     {
+        // Initialize the array to hold all the pokemons
         allFletchings = new GameObject[numFletchings];
         allTalonflames = new List<GameObject>();
+
+        // Loop to create and place each pokemon randomly within the limits
         for (int i = 0; i < numFletchings; i++)
         {
+            // Calculate a random position within the limits
             Vector3 pos = this.transform.position + new Vector3(
                 Random.Range(-limits.x, limits.x),
                 Random.Range(-limits.y, limits.y), 
                 Random.Range(-limits.z, limits.z));
+            // Instantiate the fletching prefab at the random position with no rotation
             allFletchings[i] = Instantiate(fletchingPrefab, pos, Quaternion.identity);
         }
 
         for (int i = 0; i < numTalonflames; i++)
         {
+            // Calculate a random position within the limits
             Vector3 pos = this.transform.position + new Vector3(
                 Random.Range(-limits.x, limits.x),
                 Random.Range(-limits.y, limits.y),
                 Random.Range(-limits.z, limits.z));
+            // Instantiate the talonflame prefab at the random position with no rotation
             GameObject talonflame = Instantiate(talonflamePrefab, pos, Quaternion.identity);
             allTalonflames.Add(talonflame); // Add to the leaders list
         }
 
+        // Set the static reference to this instance of FlockManager
         FM = this;
 
+        // Initialize the goal position as the FlockManager's position
         goalPos = this.transform.position;
     }
 
     // Update is called once per frame
+    // Randomly updates the goal position for the pokemons to fly towards
     void Update()
     {
         if (Random.Range(0, 100) < leaderInfluence)
@@ -70,6 +81,7 @@ public class FlockManager : MonoBehaviour
             }
             else
             {
+                // Calculate a new random goal position within the limits
                 goalPos = this.transform.position + new Vector3(
                     Random.Range(-limits.x, limits.x),
                     Random.Range(-limits.y, limits.y),  
